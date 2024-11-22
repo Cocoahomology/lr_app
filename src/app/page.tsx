88ab1d7ecc1d327c6e941cdc211f92f6e4099469
Cocoahomology <83151536+Cocoahomology@users.noreply.github.com>
2024-11-23 00:17:12 +0800

    feat: display Cryptocurrency Price Tracker on landing
    This is the minimum needed to display the cryptocurrency price tracker on the landing page with responsiveness. There is still a lot of refactoring and improvements needed to complete this feature.
    
    Cryptocurrency price tracker refreshes once every minute with the latest data.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,51 @@
-import Image from "next/image";
+"use client";
 import styles from "./page.module.css";
+import { fetchAllPrices } from "@/api/prices";
+import { useEffect, useState } from "react";
+import BarChartComponent from "@/components/ECharts/BarChart";
+import { IFetchAllPricesResponse } from "@/api/prices/types";
 
 export default function Home() {
+  // TODO: Move all of following to appropriate modules/components.
+  const [formattedChartData, setFormattedChartData] = useState<
+    Array<[string, number]>
+  >([]);
+
+  useEffect(() => {
+    const fetchData = () => {
+      fetchAllPrices().then((allPriceData) => {
+        const formattedData = formatChartData(allPriceData) as any;
+        setFormattedChartData(formattedData);
+      });
+    };
+
+    fetchData(); // Initial fetch
+    const timeout = setInterval(fetchData, 60000); // Fetch every minute
+
+    return () => clearInterval(timeout); // Cleanup on unmount
+  }, []);
+
+  function formatChartData(allPriceData: IFetchAllPricesResponse) {
+    const cryptoCurrencyList = allPriceData.cryptocurrencys;
+    return cryptoCurrencyList.map((crytpocurrency) => [
+      crytpocurrency.name,
+      crytpocurrency.price,
+    ]);
+  }
+
+  //  TODO: I think using CSS for chartContainer is not a good practice, this should be changed to a component.
   return (
     <div className={styles.page}>
+      <h1>Cryptocurrency Price Tracker</h1>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+        <div className={styles.chartContainer}>
+          <BarChartComponent
+            chartData={formattedChartData}
+            title="Current Cryptocurrency Prices"
+            yAxisLabel="Price (USD)"
+          />
         </div>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
