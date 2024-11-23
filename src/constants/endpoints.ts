e890c3502347974c5c43d731dc079dc1d3a48fef
Cocoahomology <83151536+Cocoahomology@users.noreply.github.com>
2024-11-24 06:23:35 +0800

    chore: change LR_SERVER endpoint from prod to local

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -1,5 +1,4 @@
 //API Endpoints
 
-// XXX : This endpoint is an AWS EC2 deployed instance of LR Server. Change to localhost for local development.
-export const LR_SERVER = "https://3.101.12.102:8000/graphql/";
-// export const LR_SERVER = "https://localhost:8000/graphql/";
+// XXX Endpoint is set for local development.
+export const LR_SERVER = "http://localhost:8000/graphql/";
